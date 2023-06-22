15c1d9eec431ae0471c1ba3f9b620e87a0ae714f
j3553pat <jessecodedev@gmail.com>
2023-06-21 21:05:08 -0500

    deployed

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,66 @@
 import React from 'react'
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container, Menu, MenuItem} from "@mui/material";
+import { MoreVert } from '@mui/icons-material'
 
 const Import = (props) => {
-    // fill out this component
+
+const [anchorEl, setAnchorEl] = React.useState(null);
+const open = Boolean(anchorEl);
+
+const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleDelete = () => {
+      props.deleteMake();
+      setAnchorEl(null);
+  };
+
 
     return (
-        <p>Import Component</p>
+        <Container maxWidth="lg">
+            <br />
+            <p>Import Component</p>
+            <br />
+        <Button variant="contained" color='primary' onClick={props.fetchMakes}>
+            IMPORT
+        </Button>
+        <h2>COUNT: {props.makes.length}</h2>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Make</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+        <TableBody>
+            {props.makes.map((make, index)=> (
+                <TableRow key={make.MakeId}>
+                <TableCell >{make.MakeId}</TableCell>
+                <TableCell>{make.MakeName}</TableCell>
+                <TableCell align='right'><MoreVert id="basic-button"
+							aria-controls={open ? 'basicMenu' : undefined}
+							aria-haspopup="true"
+							aria-expanded={open ? 'true' : undefined}
+							onClick={handleClick}/></TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+        </Table>
+        <Menu
+         id="basicMenu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}>
+            <MenuItem onClick={handleDelete}>Delete</MenuItem>
+        </Menu>
+    </Container>
     )
 }
 
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,20 @@
-// fill out this container
+import { fetchMakes, deleteMake } from "../redux/actions"
+import { connect } from "react-redux"
+import Import from "../components/Import"
+
+const mapStateToProps = (state) => {
+    return {
+        makes: state.makes
+    }
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchMakes: () => dispatch(fetchMakes()),
+        deleteMake: (index) => dispatch(deleteMake(index))
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Import)
